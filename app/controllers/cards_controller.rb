8f224649f2e1ad041ec1010cc455b65b8f5050ac
Aleks <merff.555@gmail.com>
2016-05-03 18:40:39 +0600

    add simple_form_for card, def show, new, create

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,4 +2,23 @@ class CardsController < ApplicationController
 	def index
 		@cards = Card.all
 	end
+
+	def show
+		@card = Card.find(params[:id])
+	end
+
+	def new
+	end
+
+	def create
+		@card = Card.new(card_params)
+
+		@card.save
+		redirect_to @card
+	end
 end
+
+private
+	def card_params
+		params.require(:card).permit(:original, :translated, :review)
+	end
