dd4605491ed5b7d5eb73f656678a1d71d9b952a1
Aleks <merff.555@gmail.com>
2016-05-05 11:05:17 +0600

    Add validation method

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,13 +1,15 @@
 class Card < ActiveRecord::Base
-	validates :original, :translated, presence: true, if: :valid_combo?
+	validates :original, :translated, presence: true
+	validate :valid_combo 
 	before_create :create_date_review
 	
-	def valid_combo?
-		:original != :translated
+	def valid_combo
+		if self.original.upcase == self.translated.upcase
+			errors.add(:translated, "translated not equal original")
+		end
 	end
 
 	def create_date_review
 		self.review = Date.today.next_day(3)
 	end
 end
-#{ case_sensitive: false }
