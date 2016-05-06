81f8b9a3a8bd68de5197a1049bc32b739f53b690
Aleks <merff.555@gmail.com>
2016-05-06 15:51:51 +0600

    New seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,10 @@
 require 'open-uri'
 page = Nokogiri::HTML(open("http://wordsteps.com/vocabulary/words/121275/%D0%A4%D1%80%D1%83%D0%BA%D1%82%D1%8B"))
-	en  = Array.new 
-	rus = Array.new
-page.xpath('//div[@class = "static word"]').each do |en_word|
-	en << en_word.text
-end
-page.xpath('//div[@class = "static trans"]').each do |rus_word|
-	rus << rus_word.text
-end
-en.size.times do |i|
-	Card.create(original: en[i], translated: rus[i])
+
+page.css("tr.wrow").each do |tr|
+	Card.create(
+		original: tr.css("div.word").text,
+		translated: tr.css("div.trans").text,
+		review: (DateTime.now.to_date + 3)
+		)
 end
