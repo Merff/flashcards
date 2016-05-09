fc16fb9d303f5c7b42f6d4decdf62e464b56f75a
Aleks <merff.555@gmail.com>
2016-05-10 02:07:34 +0600

    New tests for Card

--- app/models/card.rb
+++ app/models/card.rb
@@ -4,20 +4,22 @@ class Card < ActiveRecord::Base
 	before_create :create_date_review
 	
 	def valid_combo
-		if self.original.upcase == self.translated.upcase
+		if original.downcase == translated.downcase
 			errors.add(:translated, "translated not equal original")
 		end
 	end
 
 	def create_date_review
-		self.review = Date.today.next_day(3)
+		review = Date.today.next_day(3)
 	end
 
 	scope :random_card, -> {where("review <= ?", Date.today).order('RANDOM()').limit(1).take}
 
 	def check_translation(answer)
-		if answer.upcase == self.original.upcase
-			self.update_attributes(review: Date.today.next_day(3))
+		if answer.downcase == original.downcase
+			update_attributes(review: Date.today.next_day(3))
+		else
+			false
 		end
 	end
 
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -2,19 +2,20 @@ require 'rails_helper'
 
 describe Card do 
 	
-	it "translated text not equal original" do
-		card = Card.create(original: "space", translated: "Space")
-		expect(card.original.upcase).to eq(card.translated.upcase)
-	end
-
-	it "date review must be +3 days" do
+	it "when create, date review must be +3 days" do
 		card = Card.create(original: "space", translated: "космос")
-		expect(card.review).to eq(Date.today.next_day(3))
+		expect(card.create_date_review).to eq(Date.today.next_day(3))
 	end
 
-	it "answer must be correct" do
-		card = Card.new(original: "war")
-		answer = "WaR"
-		expect(answer.upcase).to eq(card.original.upcase)
+	it "answer right" do
+		card = Card.create(original: "war", translated: "война")
+		expect(card.check_translation("war")).to be true
+		expect(card.check_translation("star")).to be false
+	end
+
+	it "random card in home page" do
+		test_card = Card.create(original: "black", translated: "черный", review: "2016-05-09")
+		card = Card.random_card
+		expect(card.nil?).to be false
 	end
 end
